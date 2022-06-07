e8232bda6298a0fe5f98454ca5822b0ec786b56d
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-07 22:56:44 +1000

    created app and connected to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+firebase.initializeApp({
+  apiKey: "AIzaSyD2Fm7RN9Ln8o3kHyuk8Wt9uR0cP3llLGY",
+  authDomain: "ambient-decoder-288922.firebaseapp.com",
+  projectId: "ambient-decoder-288922",
+  storageBucket: "ambient-decoder-288922.appspot.com",
+  messagingSenderId: "1013077742096",
+  appId: "1:1013077742096:web:a8368e932c332ad36c3095",
+  measurementId: "G-3MWF52QYQB",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
